c1c880a419d42d2a0b7165d34ae6f327cf7f848b
Mohamed-Elsharkawy <141739269+MohamedElsharkawyy@users.noreply.github.com>
2024-01-17 15:08:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,37 @@
 let title = document.querySelector(".title");
 let divs = document.querySelectorAll(".square");
+let loading =document.querySelector('.loading');
 let turn = "x";
 let squares = [];
 function reload() {
     title.innerHTML = "Reload";
         title.style.color = "#fa0";
+
         setInterval(() => {
-            title.innerHTML += `.`;
+            title.innerHTML += ``;
         }, 1200);
+        
         setTimeout(() => {
             location.reload();
         }, 1200);
 }
 function end(num1, num2, num3) {
-    title.innerHTML = `${squares[num1]} IS WIN`;
+    loading.classList.replace('d-none', 'd-visible')
+    title.innerHTML = `${squares[num1]} IS WIN `;
+    loading.innerHTML="Please Wait"
     title.style.color = "#fa0";
     document.getElementById('item' + num1).style.background = "green";
     document.getElementById('item' + num2).style.background = "green";
     document.getElementById('item' + num3).style.background = "green";
+
     setInterval(() => {
-        title.innerHTML += `.`;
+        title.innerHTML += ``;
     }, 1000);
     setTimeout(() => {
         location.reload();
     }, 1200);
 }
+
 function winner() {
     for (let i = 1; i < 10; i++) {
         squares[i] = document.getElementById(`item${i}`).innerHTML;
@@ -32,31 +39,32 @@ function winner() {
     if (squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != "") {
         end(1, 2, 3);
         reload();
-    } else if (squares[4] == squares[5] && squares[5] == squares[6] && squares[4] != "") {
+} else if (squares[4] == squares[5] && squares[5] == squares[6] && squares[4] != "") {
         end(4, 5, 6);
         reload();
-    } else if (squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != "") {
+} else if (squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != "") {
         end(7, 8, 9);
         reload();
-    } else if (squares[1] == squares[5] && squares[5] == squares[9] && squares[1] != "") {
+} else if (squares[1] == squares[5] && squares[5] == squares[9] && squares[1] != "") {
         end(1, 5, 9);
         reload();
-    } else if (squares[3] == squares[5] && squares[5] == squares[7] && squares[3] != "") {
+} else if (squares[3] == squares[5] && squares[5] == squares[7] && squares[3] != "") {
         end(3, 5, 7);
         reload();
-    } else if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != "") {
+} else if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != "") {
         end(1, 4, 7);
         reload();
-    } else if (squares[2] == squares[5] && squares[5] == squares[8] && squares[2] != "") {
+} else if (squares[2] == squares[5] && squares[5] == squares[8] && squares[2] != "") {
         end(2, 5, 8);
         reload();
     } else if (squares[3] == squares[6] && squares[6] == squares[9] && squares[3] != "") {
         end(3, 6, 9);
         reload();
-    } else if (squares[1] != "" && squares[2] != "" && squares[3] != "" && squares[4] != "" && squares[5] != "" && squares[6] != "" && squares[7] != "" && squares[8] != "" && squares[9] != "") {
+} else if (squares[1] != "" && squares[2] != "" && squares[3] != "" && squares[4] != "" && squares[5] != "" && squares[6] != "" && squares[7] != "" && squares[8] != "" && squares[9] != "") {
         reload();   
     }
 }
+
 divs.forEach((e) => {
     e.addEventListener('click', () => {
         if (turn === "x" && e.innerHTML === "") {
